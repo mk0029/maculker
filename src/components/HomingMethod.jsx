57ecad8c8fdda2600784c342827fe476fb50ad81
mk0029 <mkhicher404@gmail.com>
2023-06-06 12:04:35 +0530

    Add

---
+++ src/components/HomingMethod.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HomingMethod() {
+  return (
+    <>
+      <section className="py-5">
+        <div className=" container">
+          <div className="row"></div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default HomingMethod;
