fc7c53c6a841b4a3908c0be9a108f2f6eaab1ea8
mk0029 <mkhicher404@gmail.com>
2023-06-06 15:35:16 +0530

    //\

--- src/components/PatientsRecovered.jsx
+++ src/components/PatientsRecovered.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Recover__ from "../assets/img/png/Recover.png";
 import TextArrow from "../assets/img/svg/Arrowpatint.svg";
+import Eyepos from "../assets/img/svg/EYEPOS.svg";
+import Goglepos from "../assets/img/svg/GoogelsPos.svg";
 
 function PatientsRecovered() {
   return (
@@ -11,16 +13,38 @@ function PatientsRecovered() {
           {" "}
           <div className=" container">
             <div className="row justify-content-between align-items-center">
-              <div className="col-5 h-100 ">
-                <div className="">
+              <div className="col-xl-5 col-lg-6 h-100 ">
+                <div className=" position-relative">
+                  <div
+                    style={{ maxWidth: "207px" }}
+                    className="box_Patint_Pis1 position-absolute rounded-3 d-flex gap-xl-4 gap-3 p-2 ">
+                    <img src={Eyepos} alt="" />
+                    <h5 className=" ff_poppins fw-normal fs_md opacity_07  text__lightwhite mb-0">
+                      Macular Degeneration
+                    </h5>
+                  </div>
+                  <div
+                    style={{ maxWidth: "207px" }}
+                    className="box_Patint_Pis2 position-absolute px-3 py-4 rounded-3">
+                    <div className="d-flex gap-2 align-items-center">
+                      <img src={Goglepos} alt="" />
+                      <h5 className="mb-0  ff_poppins fw-semibold fs_md  text__lightwhite">
+                        Patient Recover
+                      </h5>
+                    </div>
+                    <p className=" ff_poppins fw-normal fs_md opacity_09  text__lightwhite">
+                      We are using an injection to stop the growth factor of
+                      Retina
+                    </p>
+                  </div>
                   <img className="Homing_Method" src={Recover__} alt="" />
                 </div>
               </div>
-              <div className="col-6">
+              <div className="col-lg-6 mt-4 mt-lg-0">
                 <div className="">
                   <div className=" position-relative d-inline-block">
                     <img
-                      className=" position-absolute Arrow_Pat"
+                      className=" position-absolute d-none d-md-block Arrow_Pat"
                       src={TextArrow}
                       alt=""
                     />{" "}
@@ -39,7 +63,7 @@ function PatientsRecovered() {
                     suffering from dry AMD and temporary vision loss. Our
                     strategies cured her and now she is living happily.
                   </p>
-                  <span className=" d-inline-block pt-4 mt-3">
+                  <span className=" d-inline-block pt-md-4 pt-3 mt-xl-3">
                     <a className=" common_btn" href="">
                       Book Call
                     </a>
