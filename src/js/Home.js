050c776459fd9514501b2ab2cb17cebcf9b68ce4
Mj-Saini <manojsaini97285@gmail.com>
2023-06-06 16:32:14 +0530

    footer

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,15 +1,24 @@
 import React from "react";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Disease from "../components/Disease";
 import Program from "../components/Program";
+import Fees from "../components/Fees";
+import Eligible from "../components/Eligible";
+import Faq from "../components/Faq";
+import Review from "../components/Review";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
-    <div className="bg_black">
+    <div className="bg_black overflow-hidden">
       <Hero />
       <Disease />
+      <Eligible />
+      <Fees />
       <Program />
+      <Review />
+      <Faq />
+      <Footer />
     </div>
   );
 }
